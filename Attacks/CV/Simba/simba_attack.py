1cf44338703a0a02e96275a0f97a283ceb3a76c6
Ahmed Tamer <143439432+AhmedTamergithub@users.noreply.github.com>
2025-03-28 06:40:19 +0200

    Update simba_attack.py

--- Attacks/CV/Simba/simba_attack.py
+++ Attacks/CV/Simba/simba_attack.py
@@ -79,4 +79,14 @@ def simba_attack(image_tensor, true_label, model, num_iters=2000, epsilon=20, pr
     # Return the adversarial image as a TensorFlow tensor
     adv_tensor = tf.convert_to_tensor(adv, dtype=tf.float32)
     return adv_tensor
-
+# Example usage:
+image_path = "Restricted.ppm"
+model = tf.keras.models.load_model("TrafficSigns_EfficientNetB1.keras")  # Load your trained model
+image_tensor = load_ppm_image(image_path, target_size=(240, 240))
+true_label = predict_traffic_sign(image_tensor, model, GTSRB_CLASSES)
+adv_image_tensor = simba_attack(image_tensor, true_label, model, num_iters=100, epsilon=50, print_every=10)
+#
+# # Optionally, save or visualize the adversarial image:
+adv_image = adv_image_tensor.numpy()[0].astype(np.uint8)
+adv_pil = Image.fromarray(adv_image)
+adv_pil.save("Restriction_adv_image.ppm")

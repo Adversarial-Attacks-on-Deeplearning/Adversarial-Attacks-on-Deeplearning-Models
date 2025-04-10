7a5b7ae09387125bf420d394214c2f01d8763bc2
Youssef-Abuzeid <yousifh991@gmail.com>
2025-04-10 16:15:57 +0200

    Fix alpha scalling

--- Attacks/CV/PGD.py
+++ Attacks/CV/PGD.py
@@ -19,13 +19,14 @@ def pgd_attack_single_image(model, image, label, epsilon, alpha, iterations, nor
     # Adjust epsilon for non normalized images
     if not normalized:
         epsilon = epsilon * 255.0
+        alpha = alpha*255.0
     # Ensure the image has a batch dimension
     image = tf.convert_to_tensor(image, dtype=tf.float32)
     image = tf.expand_dims(image, axis=0)  # Add batch dimension
     label = tf.expand_dims(label, axis=0)  # Add batch dimension
 
-    # Initialize adversarial image with the original image
-    adversarial_image = tf.identity(image)
+    noise = tf.random.uniform(shape=tf.shape(image), minval=-epsilon, maxval=epsilon)
+    adversarial_image = tf.clip_by_value(image + noise, 0, 255)
 
     for _ in range(iterations):
         with tf.GradientTape() as tape:
@@ -33,13 +34,13 @@ def pgd_attack_single_image(model, image, label, epsilon, alpha, iterations, nor
             # Forward pass
             prediction = model(adversarial_image, training=False)
             # Calculate loss
-            loss = tf.keras.losses.sparse_categorical_crossentropy(label, prediction)
+            loss =  tf.keras.losses.sparse_categorical_crossentropy(label, prediction)
         
         # Calculate gradient of the loss w.r.t. the adversarial image
         gradient = tape.gradient(loss, adversarial_image)
 
         # Get the sign of the gradient and update the adversarial image
-        adversarial_image = adversarial_image + alpha * tf.sign(gradient)
+        adversarial_image = adversarial_image  + alpha * tf.sign(gradient)
 
         # Project the adversarial image into the epsilon-ball and clip to [0, 255]
         adversarial_image = tf.clip_by_value(adversarial_image, image - epsilon, image + epsilon)
